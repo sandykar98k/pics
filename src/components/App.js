cfdd1f9f5ed4f432b875a1c763aac229137c4f40
sandykar98k <iambansalsandeep@gmail.com>
2018-12-19 20:25:06 +0530

    Child to parent communication

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,18 @@ import React from 'react';
 // Components
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: 10 }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  onSearchSubmit = term => {
+    console.log(term);
+  };
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: 10 }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
