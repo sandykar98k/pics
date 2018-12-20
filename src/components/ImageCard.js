054537d8be9df8227f3f192f637fbed8a0b8b88b
sandykar98k <iambansalsandeep@gmail.com>
2018-12-20 13:45:26 +0530

    Finishing the app

---
+++ src/components/ImageCard.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { spans: 0 };
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+
+    // console.log(this.imageRef.current.clientHeight); // It return 0 becuase at the time of this image is not loaded. to get correct height we use load event listener.
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+    this.setState({ spans });
+  };
+
+  render() {
+    const { description, urls } = this.props.image;
+    return (
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
+      </div>
+    );
+  }
+}
+
+export default ImageCard;
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,17 +1,13 @@
 import React from 'react';
+import './ImageList.css';
+import ImageCard from './ImageCard';
 
 const ImageList = props => {
   const images = props.images.map(image => {
-    return (
-      <img
-        key={image.id}
-        src={image.urls.regular}
-        alt={props.images.description}
-      />
-    );
+    return <ImageCard key={image.id} image={image} />;
   });
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
